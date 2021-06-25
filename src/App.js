5233e033663902107de3bc030646bdd2b53a9c6a
Ramiro Méndez López <=>
2021-06-25 02:58:10 -0500

    handle audio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,188 @@
+import { useState, useRef } from 'react';
+import { SpotifyWebApi, getIntervals, getPeaks } from './util';
+
 const App = () => {
+    const [track, setTrack] = useState('');
+    const [result, setResult] = useState({});
+    const audio = useRef();
+    const spotifyApi = new SpotifyWebApi();
+
+    spotifyApi.getToken().then(function (response) {
+        spotifyApi.setAccessToken(response.token);
+    });
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        spotifyApi.searchTracks(event.target.song.value.trim(), { limit: 1 })
+            .then((response) => {
+                const track = response.tracks.items[0];
+                const previewUrl = track.preview_url;
+                setTrack(track.preview_url);
+                var request = new XMLHttpRequest();
+                request.open('GET', previewUrl, true);
+                request.responseType = 'arraybuffer';
+                request.onload = function () {
+
+                    // Create offline context
+                    var OfflineContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
+                    var offlineContext = new OfflineContext(2, 30 * 44100, 44100);
+
+                    offlineContext.decodeAudioData(request.response, function (buffer) {
+
+                        // Create buffer source
+                        var source = offlineContext.createBufferSource();
+                        source.buffer = buffer;
+
+                        // Beats, or kicks, generally occur around the 100 to 150 hz range.
+                        // Below this is often the bassline.  So let's focus just on that.
+
+                        // First a lowpass to remove most of the song.
+
+                        var lowpass = offlineContext.createBiquadFilter();
+                        lowpass.type = "lowpass";
+                        lowpass.frequency.value = 150;
+                        lowpass.Q.value = 1;
+
+                        // Run the output of the source through the low pass.
+
+                        source.connect(lowpass);
+
+                        // Now a highpass to remove the bassline.
+
+                        var highpass = offlineContext.createBiquadFilter();
+                        highpass.type = "highpass";
+                        highpass.frequency.value = 100;
+                        highpass.Q.value = 1;
+
+                        // Run the output of the lowpass through the highpass.
+
+                        lowpass.connect(highpass);
+
+                        // Run the output of the highpass through our offline context.
+
+                        highpass.connect(offlineContext.destination);
+
+                        // Start the source, and render the output into the offline conext.
+
+                        source.start(0);
+                        offlineContext.startRendering();
+                    });
+
+                    offlineContext.oncomplete = function (e) {
+                        var buffer = e.renderedBuffer;
+                        var peaks = getPeaks([buffer.getChannelData(0), buffer.getChannelData(1)]);
+                        var groups = getIntervals(peaks);
+
+                        var svg = document.querySelector('#svg');
+                        svg.innerHTML = '';
+                        var svgNS = 'http://www.w3.org/2000/svg';
+                        var rect;
+                        peaks.forEach(function (peak) {
+                            rect = document.createElementNS(svgNS, 'rect');
+                            rect.setAttributeNS(null, 'x', (100 * peak.position / buffer.length) + '%');
+                            rect.setAttributeNS(null, 'y', 0);
+                            rect.setAttributeNS(null, 'width', 1);
+                            rect.setAttributeNS(null, 'height', '100%');
+                            svg.appendChild(rect);
+                        });
+
+                        rect = document.createElementNS(svgNS, 'rect');
+                        rect.setAttributeNS(null, 'id', 'progress');
+                        rect.setAttributeNS(null, 'y', 0);
+                        rect.setAttributeNS(null, 'width', 1);
+                        rect.setAttributeNS(null, 'height', '100%');
+                        svg.appendChild(rect);
+
+                        svg.innerHTML = svg.innerHTML; // force repaint in some browsers
+
+                        var top = groups.sort(function (intA, intB) {
+                            return intB.count - intA.count;
+                        }).splice(0, 5);
+
+                        spotifyApi.getAudioFeaturesForTrack(track.id)
+                            .then((audioFeatures) => {
+                                setResult({ track, top, tempo: audioFeatures.tempo });
+                            });
+                    };
+                };
+                request.send();
+            });
+
+    }
+
+    function updateProgressState() {
+        if (audio.paused) {
+            return;
+        }
+        var progressIndicator = document.querySelector('#progress');
+        if (progressIndicator && audio.duration) {
+            progressIndicator.setAttribute('x', (audio.currentTime * 100 / audio.duration) + '%');
+        }
+        requestAnimationFrame(updateProgressState);
+    }
+
+    const HandleResult = () => {
+        return (
+            <>
+                <div className="text-lg sm:text-xl">
+                    <div>Guess for track <strong>{result.track.name}</strong> by <strong>{result.track.artists[0].name}</strong> is <strong>
+                        {Math.round(result.top[0].tempo)} BPM</strong> with {result.top[0].count} samples.</div>
+
+                    <div class="small">Other options are [
+                        {result.top.slice(1).map((e) => {
+                            return e.tempo + ' BPM (' + e.count + ')';
+                        }).join(', ')}]
+                    </div>
+                </div>
+
+                <div class="small">The tempo according to Spotify is {result.tempo} BPM</div>
+            </>
+        );
+    }
+
+    // audio.addEventListener('playing', updateProgressState);
+    
+    const handlePlay = () => {
+        audio.play();
+
+        // audio.paused ? audio.play() : audio.pause();
+        // if (audio.paused) {
+        //     audio.play();
+        // } else {
+        //     audio.pause();
+        // }
+        updateProgressState();
+    };
+
     return (
-        <section className="flex h-screen w-screen bg-gray-700 justify-center items-center">
-            <div className="text-center">
-                <h1 className="text-5xl font-semibold text-gray-400">Tailwind x React</h1>
-                <h2><a className="text-gray-500" 
-                href="https://github.com/RamiroG8k" target="_blank" rel="noreferrer">RamiroG8k</a> template</h2>
+        <section className="p-10 sm:py-10 sm:px-36 h-screen w-screen bg-gray-700 justify-center items-center">
+            <div className="text-center sm:my-10">
+                <h1 className="text-4xl sm:text-5xl font-bold text-gray-400 my-3">Finding out the BPM of a song using Javascript</h1>
+                <p className="text-lg leading-tight sm:text-2xl text-gray-900 font-semibold">This demo uses the browser's <strong>Audio API</strong> to determine the tempo of a song, processing a chunk of
+                    30 seconds of a song.</p>
+            </div>
+
+            <form onSubmit={handleSubmit} className="sm:flex my-5 sm:my-10 gap-5">
+                <div className="w-full">
+                    <input className="block w-full p-3 rounded-t-2xl focus:outline-none text-center" name="song"
+                        placeholder="Type the name of a track (e.g. Beyonce - Baby Boy)" />
+                    <button className="block w-full bg-gray-400 p-3 rounded-b-2xl focus:outline-none" type="submit">
+                        Search track &amp; Calculate tempo
+                    </button>
+                </div>
+                <div className="flex my-2 sm:m-0 sm:w-2/12 justify-center items-center">
+                    {track && <button OnClick={() => handlePlay()} 
+                    className={track ? "block" : "hidden", "flex-1 h-full bg-gray-600 p-2 rounded-2xl focus:outline-none"}>{audio.paused ? 'Play track' : 'Pause track'}</button>}
+                </div>
+            </form>
+
+            <div>
+                <div className="text-center text-gray-900 font-semibold">
+                    {result.track && <HandleResult />}
+                </div>
+                {track && <svg className="w-full bg-gray-500 h-16 my-4" id="svg"></svg>}
+                <audio ref={audio} src={track}></audio>
             </div>
         </section>
     );
---
+++ src/util/index.js
@@ -0,0 +1,266 @@
+export var SpotifyWebApi = (() => {
+
+    var _baseUri = 'https://api.spotify.com/v1';
+    var _baseTokenUri = 'https://spotify-web-api-token.herokuapp.com';
+    var _accessToken = null;
+
+    var _promiseProvider = function (promiseFunction) {
+        return new window.Promise(promiseFunction);
+    };
+
+    var _checkParamsAndPerformRequest = function (requestData, options, callback) {
+        var opt = {};
+        var cb = null;
+
+        if (typeof options === 'object') {
+            opt = options;
+            cb = callback;
+        } else if (typeof options === 'function') {
+            cb = options;
+        }
+        _extend(requestData.params, opt);
+        return _performRequest(requestData, cb);
+    };
+
+    var _performRequest = function (requestData, callback) {
+        var promiseFunction = function (resolve, reject) {
+            var req = new XMLHttpRequest();
+            var type = requestData.type || 'GET';
+            if (type === 'GET') {
+                req.open(type,
+                    _buildUrl(requestData.url, requestData.params),
+                    true);
+            } else {
+                req.open(type, _buildUrl(requestData.url));
+            }
+            if (_accessToken) {
+                req.setRequestHeader('Authorization', 'Bearer ' + _accessToken);
+            }
+            req.onreadystatechange = function () {
+                if (req.readyState === 4) {
+                    var data = null;
+                    try {
+                        data = req.responseText ? JSON.parse(req.responseText) : '';
+                    } catch (e) { }
+
+                    if (req.status === 200 || req.status === 201) {
+                        if (resolve) {
+                            resolve(data);
+                        }
+                        if (callback) {
+                            callback(null, data);
+                        }
+                    } else {
+                        if (reject) {
+                            reject(req);
+                        }
+                        if (callback) {
+                            callback(req, null);
+                        }
+                    }
+                }
+            };
+
+            if (type === 'GET') {
+                req.send(null);
+            } else {
+                req.send(JSON.stringify(requestData.postData));
+            }
+        };
+
+        if (callback) {
+            promiseFunction();
+            return null;
+        } else {
+            return _promiseProvider(promiseFunction);
+        }
+    };
+
+    var _extend = function () {
+        var args = Array.prototype.slice.call(arguments);
+        var target = args[0];
+        var objects = args.slice(1);
+        target = target || {};
+        for (var i = 0; i < objects.length; i++) {
+            for (var j in objects[i]) {
+                target[j] = objects[i][j];
+            }
+        }
+        return target;
+    };
+
+    var _buildUrl = function (url, parameters) {
+        var qs = '';
+        for (var key in parameters) {
+            if (parameters.hasOwnProperty(key)) {
+                var value = parameters[key];
+                qs += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
+            }
+        }
+        if (qs.length > 0) {
+            qs = qs.substring(0, qs.length - 1); //chop off last '&'
+            url = url + '?' + qs;
+        }
+        return url;
+    };
+
+    var Constr = function () { };
+
+    Constr.prototype = {
+        constructor: SpotifyWebApi
+    };
+
+    /**
+     * Sets the access token to be used.
+     * See [the Authorization Guide](https://developer.spotify.com/web-api/authorization-guide/) on
+     * the Spotify Developer site for more information about obtaining an access token.
+     * @param {string} accessToken The access token
+     * @return {void}
+     */
+    Constr.prototype.setAccessToken = function (accessToken) {
+        _accessToken = accessToken;
+    };
+
+    /**
+     * Fetches tracks from the Spotify catalog according to a query.
+     * See [Search for an Item](https://developer.spotify.com/web-api/search-item/) on
+     * the Spotify Developer site for more information about the endpoint.
+     * @param {Object} options A JSON object with options that can be passed
+     * @param {function(Object, Object)} callback An optional callback that receives 2 parameters. The first
+     * one is the error object (null if no error), and the second is the value if the request succeeded.
+     * @return {Object} Null if a callback is provided, a `Promise` object otherwise
+     */
+    Constr.prototype.searchTracks = function (query, options, callback) {
+        var requestData = {
+            url: _baseUri + '/search/',
+            params: {
+                q: query,
+                type: 'track'
+            }
+        };
+        return _checkParamsAndPerformRequest(requestData, options, callback);
+    };
+
+    /**
+     * Get audio features for a single track identified by its unique Spotify ID.
+     * See [Get Audio Features for a Track](https://developer.spotify.com/web-api/get-audio-features/) on
+     * the Spotify Developer site for more information about the endpoint.
+     * @param {string} trackId The id of the track. If you know the Spotify URI it is easy
+     * to find the track id (e.g. spotify:track:<here_is_the_track_id>)
+     * @param {function(Object,Object)} callback An optional callback that receives 2 parameters. The first
+     * one is the error object (null if no error), and the second is the value if the request succeeded.
+     * @return {Object} Null if a callback is provided, a `Promise` object otherwise
+     */
+    Constr.prototype.getAudioFeaturesForTrack = function (trackId, callback) {
+        var requestData = {
+            url: _baseUri + '/audio-features/' + trackId
+        };
+        return _checkParamsAndPerformRequest(requestData, {}, callback);
+    };
+
+    /**
+     * Obtains a token to be used against the Spotify Web API
+     */
+    Constr.prototype.getToken = function (callback) {
+        var requestData = {
+            url: _baseTokenUri + '/token'
+        };
+        return _checkParamsAndPerformRequest(requestData, {}, callback);
+    };
+
+    return Constr;
+})();
+
+export function getPeaks(data) {
+
+    // What we're going to do here, is to divide up our audio into parts.
+
+    // We will then identify, for each part, what the loudest sample is in that
+    // part.
+
+    // It's implied that that sample would represent the most likely 'beat'
+    // within that part.
+
+    // Each part is 0.5 seconds long - or 22,050 samples.
+
+    // This will give us 60 'beats' - we will only take the loudest half of
+    // those.
+
+    // This will allow us to ignore breaks, and allow us to address tracks with
+    // a BPM below 120.
+
+    var partSize = 22050,
+        parts = data[0].length / partSize,
+        peaks = [];
+
+    for (var i = 0; i < parts; i++) {
+        var max = 0;
+        for (var j = i * partSize; j < (i + 1) * partSize; j++) {
+            var volume = Math.max(Math.abs(data[0][j]), Math.abs(data[1][j]));
+            if (!max || (volume > max.volume)) {
+                max = {
+                    position: j,
+                    volume: volume
+                };
+            }
+        }
+        peaks.push(max);
+    }
+
+    // We then sort the peaks according to volume...
+
+    peaks.sort(function (a, b) {
+        return b.volume - a.volume;
+    });
+
+    // ...take the loundest half of those...
+
+    peaks = peaks.splice(0, peaks.length * 0.5);
+
+    // ...and re-sort it back based on position.
+
+    peaks.sort(function (a, b) {
+        return a.position - b.position;
+    });
+
+    return peaks;
+}
+
+export function getIntervals(peaks) {
+
+    // What we now do is get all of our peaks, and then measure the distance to
+    // other peaks, to create intervals.  Then based on the distance between
+    // those peaks (the distance of the intervals) we can calculate the BPM of
+    // that particular interval.
+
+    // The interval that is seen the most should have the BPM that corresponds
+    // to the track itself.
+
+    var groups = [];
+
+    peaks.forEach(function (peak, index) {
+        for (var i = 1; (index + i) < peaks.length && i < 10; i++) {
+            var group = {
+                tempo: (60 * 44100) / (peaks[index + i].position - peak.position),
+                count: 1
+            };
+
+            while (group.tempo < 90) {
+                group.tempo *= 2;
+            }
+
+            while (group.tempo > 180) {
+                group.tempo /= 2;
+            }
+
+            group.tempo = Math.round(group.tempo);
+
+            if (!(groups.some(function (interval) {
+                return (interval.tempo === group.tempo ? interval.count++ : 0);
+            }))) {
+                groups.push(group);
+            }
+        }
+    });
+    return groups;
+}
